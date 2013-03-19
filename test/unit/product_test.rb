244660e81b77f1b731752eeb4c03772e966df3d7
bon27 <jingkai64@gmail.com>
2013-03-19 09:32:57 +0800

    add a unit test for title uniqueness with fixtures

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+ fixtures :products
   # test "the truth" do
   #   assert true
   # end
@@ -50,7 +51,14 @@ class ProductTest < ActiveSupport::TestCase
 	end
    end
    
-
+  test "product is not valid without a unique title" do
+    product = Product.new(:title => products(:ruby).title,
+                          :description => 'yyy',
+  						  :price => 1,
+    					  :image_url => 'fred.gif')
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
+  end
 
 end
 
