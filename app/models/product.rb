87f75dcee98f87b51e9fdb4f264b62a8fcdabf7c
bon27 <jingkai64@gmail.com>
2013-03-19 10:01:31 +0800

    add a validation to models to check the title length,and add an error message

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :price, :presence=>true
   validates :title, :uniqueness=>true
+  validates :title, :length => {:minimum=>10,:message=>"you sucked,boy"}
   validates :price, :numericality=>{:greater_than_or_equal_to=>0.01}
   validates :image_url, :format=>{
     :with => %r{\.(gif|jpg|png)$}i,
