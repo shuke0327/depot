922e1a6cfab12ecbc9de6413b3289c2699c00b93
bon27 <jingkai64@gmail.com>
2013-03-19 08:50:42 +0800

    add a unit test for image_url

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -27,5 +27,28 @@ class ProductTest < ActiveSupport::TestCase
 
 	product.price = 1
 	assert product.valid?
-	
- end
+  end
+
+  def new_product(image_url) 
+    Product.new(:title => 'My Book Title',
+	            :description => 'yyy',
+				:price => 23,
+				:image_url => image_url)
+  end
+  
+  test "image url" do
+    ok = %w{fred.gif nma.gif yes_it_is.jPg me.png whonews.Gif
+	        http://2/3/4/5/fred.gif}
+	bad = %w{fred.doc fred.gif/more fred.gif.more}
+
+	ok.each do |name|
+	  assert  new_product(name).valid?, "#{name} shouldn't be invalid"
+	end
+
+	bad.each do |name|
+	  assert new_product(name).invalid?, "#{name} should't be valid"
+	end
+   end
+
+
+end
